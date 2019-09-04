ed014e520fe9fd15e8806689c5c999e266b92acb
sofiagarcia09 <sofygarcia9@gmail.com>
2019-09-03 21:10:49 -0500

    Primeros pasos con Google Maps

---
+++ app/src/main/java/com/mnqobi/actividadgooglemaps/FirstMapFragment.java
@@ -0,0 +1,62 @@
+package com.mnqobi.actividadgooglemaps;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class FirstMapFragment extends SupportMapFragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+
+    public FirstMapFragment() {
+        // Required empty public constructor
+    }
+
+    public static FirstMapFragment newInstance() {
+        return new FirstMapFragment();
+    }
+
+
+
+
+    public static FirstMapFragment newInstance(String param1, String param2) {
+        FirstMapFragment fragment = new FirstMapFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View vistaMaps = super.onCreateView(inflater, container, savedInstanceState);
+        return vistaMaps;
+    }
+
+}
--- app/src/main/java/com/mnqobi/actividadgooglemaps/MainActivity.java
+++ app/src/main/java/com/mnqobi/actividadgooglemaps/MainActivity.java
@@ -4,11 +4,55 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private FirstMapFragment mFirstMapFragment;
+
+    public static final  int LOCALTION_REQUEST_CODE = 1;
+    private GoogleMap googleMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFirstMapFragment = FirstMapFragment.newInstance();
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.map_container, mFirstMapFragment)
+                .commit();
+        mFirstMapFragment.getMapAsync(this); //registrar la aplicacion como escucha
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) { //Carga el mapa
+
+        UiSettings uiSettings = googleMap.getUiSettings();
+        uiSettings.setScrollGesturesEnabled(true);
+        uiSettings.setTiltGesturesEnabled(true);
+        uiSettings.setCompassEnabled(true);
+        uiSettings.setMyLocationButtonEnabled(true);
+        uiSettings.setMapToolbarEnabled(true);
+
+
+
+        LatLng miCasa = new LatLng(19.42667,-100.35269);
+        googleMap.addMarker(new MarkerOptions()
+        .position(miCasa)
+        .title("Casa de Sofía"));
+
+        CameraPosition cameraPosition = CameraPosition.builder()
+                .target(miCasa)
+                .zoom(10)
+                .build();
+        googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
     }
 }
